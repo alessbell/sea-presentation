3c318a1045683cd5d2ea1e8c35aae429baa39088
alessia <alessia@apollographql.com>
2022-09-20 19:51:17 -0400

    scaffold slides

---
+++ theme.js
@@ -0,0 +1,17 @@
+import { hack as theme } from 'mdx-deck/themes';
+import nightOwl from 'prism-react-renderer/themes/nightOwl';
+
+export default {
+  ...theme,
+  codeSurfer: {
+    ...nightOwl,
+    showNumbers: false,
+  },
+  li: {
+    margin: '2rem',
+  },
+};
+
+// Read the docs for more info:
+// https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
+// https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
